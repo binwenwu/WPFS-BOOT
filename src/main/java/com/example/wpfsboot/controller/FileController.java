4f41b8467c1b9cd70681873383f58cef75ea0238
Binwen Wu <2020302131043@whu.edu.cn>
2023-07-10 16:27:58 +0800

    feat:服务器连接接口

--- src/main/java/com/example/wpfsboot/controller/FileController.java
+++ src/main/java/com/example/wpfsboot/controller/FileController.java
@@ -298,9 +298,9 @@ public class FileController {
     @PostMapping("/forecast")
     public Result update(@RequestBody String fileName) {
 
-        String host = "125.220.153.23"; // 远程服务器IP地址
+        String host = "10.101.240.60"; // 远程服务器IP地址
         String user = "root"; // 远程服务器用户名
-        String password = "geopwlmars69"; // 远程服务器密码
+        String password = "ypfamily"; // 远程服务器密码
         // 要执行的命令
         StringBuilder command = new StringBuilder("pwd;");
         command.append("ls -la;");
